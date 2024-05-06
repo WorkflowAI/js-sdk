3d95797516bf3e9a309131bcb1c0ec2fa3a5ba76
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-06 14:32:59 -0400

    chore: setup lint and build scripts

---
+++ configs/eslint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: { browser: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'prettier', Since configs and plugins are installed in sub folders, this needs to be defined locally in package.json
+  ],
+  ignorePatterns: [],
+  // parser: '@typescript-eslint/parser', Since configs and plugins are installed in sub folders, this needs to be defined locally in package.json
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'simple-import-sort',
+    'unused-imports',
+  ],
+  rules: {
+    'import/order': 0, // turn off in favor of eslint-plugin-simple-import-sort
+    'import/no-unresolved': 0,
+    'import/no-duplicates': 1,
+
+    /**
+     * eslint-plugin-simple-import-sort @see https://github.com/lydell/eslint-plugin-simple-import-sort
+     */
+    'sort-imports': 0, // we use eslint-plugin-import instead
+    'simple-import-sort/imports': 1,
+    'simple-import-sort/exports': 1,
+  },
+}
