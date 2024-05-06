b4e1f89032506b592db98a3108c36d06d0dc2713
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-06 16:17:47 -0400

    wip: init client lib and types

---
+++ packages/client/src/Task.d.ts
@@ -0,0 +1,36 @@
+import type z from 'zod'
+
+import type { RunTaskOptions } from './WorkflowAI'
+
+type TaskId = string
+export type InputSchema = z.ZodTypeAny
+export type OutputSchema = z.ZodTypeAny
+
+export type TaskDefinition<IS extends InputSchema, OS extends OutputSchema> = {
+  taskId: TaskId
+  schema: {
+    input: IS
+    output: OS
+  }
+}
+
+type ExecutableTask<IS extends InputSchema, OS extends OutputSchema> = (
+  input: TaskInput<IS>,
+  options?: RunTaskOptions,
+) => Promise<TaskOutput<OS>>
+
+export type TaskInput<T> = T extends InputSchema
+  ? z.input<T>
+  : T extends TaskDefinition<infer IS, infer _OS>
+    ? TaskInput<IS>
+    : T extends ExecutableTask<infer IS, infer _OS>
+      ? TaskInput<IS>
+      : never
+
+export type TaskOutput<T> = T extends OutputSchema
+  ? z.output<T>
+  : T extends TaskDefinition<infer _IS, infer OS>
+    ? TaskOutput<OS>
+    : T extends ExecutableTask<infer _IS, infer OS>
+      ? TaskOutput<OS>
+      : never
