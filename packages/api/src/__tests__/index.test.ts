f96d389fc3b5f37d4bd32278729f93d23abba64f
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-08 14:20:50 -0400

    test: setup basic tests

---
+++ packages/api/src/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@jest/globals'
+
+import * as mainExports from '..'
+import { initWorkflowAIApi } from '../WorkflowAIApi'
+
+test('export api init function', () => {
+  expect(mainExports.initWorkflowAIApi).toEqual(initWorkflowAIApi)
+})
