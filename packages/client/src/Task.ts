3c17f1864ccb439bfefbe641f5ce4d10d85dce57
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-13 16:06:42 -0400

    feat: import task run, upsert task

--- packages/client/src/Task.ts
+++ packages/client/src/Task.ts
@@ -1,24 +1,53 @@
+import type { WorkflowAIApi } from '@workflowai/api'
 import type { z } from '@workflowai/schema'
 
-import type { RunTaskOptions } from './WorkflowAI'
+import type { ImportTaskRunOptions, RunTaskOptions } from './WorkflowAI'
 
 type TaskId = string
 export type InputSchema = z.ZodTypeAny
 export type OutputSchema = z.ZodTypeAny
 
-export type TaskDefinition<IS extends InputSchema, OS extends OutputSchema> = {
+type TaskSchema<
+  IS extends InputSchema,
+  OS extends OutputSchema,
+  OptionalId extends boolean = false,
+> = {
+  input: IS
+  output: OS
+} & (OptionalId extends true
+  ? { id?: number | null | undefined }
+  : { id: number })
+
+export type TaskDefinition<
+  IS extends InputSchema,
+  OS extends OutputSchema,
+  OptionalSchemaId extends boolean = false,
+> = {
   taskId: TaskId
-  schema: {
-    id: number
-    input: IS
-    output: OS
-  }
+  taskName?: string | null | undefined
+  schema: TaskSchema<IS, OS, OptionalSchemaId>
 }
 
-export type ExecutableTask<IS extends InputSchema, OS extends OutputSchema> = (
+export function hasSchemaId<IS extends InputSchema, OS extends OutputSchema>(
+  taskDef: TaskDefinition<IS, OS, true | false>,
+): taskDef is TaskDefinition<IS, OS, false> {
+  return taskDef.schema.id != null
+}
+
+export type ExecutableTask<IS extends InputSchema, OS extends OutputSchema> = ((
   input: TaskInput<IS>,
   options?: Partial<RunTaskOptions>,
-) => Promise<TaskOutput<OS>>
+) => Promise<TaskOutput<OS>>) & {
+  importRun: (
+    input: TaskInput<IS>,
+    output: TaskOutput<OS>,
+    options?: Partial<ImportTaskRunOptions>,
+  ) => Promise<
+    Awaited<
+      ReturnType<WorkflowAIApi['tasks']['schemas']['runs']['import']>
+    >['data']
+  >
+}
 
 export type TaskInput<T> = T extends InputSchema
   ? z.input<T>
--- packages/client/src/WorkflowAI.ts
+++ packages/client/src/WorkflowAI.ts
@@ -4,13 +4,15 @@ import {
   TaskSchemaRunGroup,
   WorkflowAIApi,
 } from '@workflowai/api'
+import { inputZodToSchema, outputZodToSchema } from '@workflowai/schema'
 
-import type {
-  ExecutableTask,
-  InputSchema,
-  OutputSchema,
-  TaskDefinition,
-  TaskOutput,
+import {
+  type ExecutableTask,
+  hasSchemaId,
+  type InputSchema,
+  type OutputSchema,
+  type TaskDefinition,
+  type TaskOutput,
 } from './Task'
 
 export type WorkflowAIConfig = {
@@ -20,6 +22,11 @@ export type WorkflowAIConfig = {
 export interface RunTaskOptions {
   group: TaskSchemaRunGroup
 }
+
+export type ImportTaskRunOptions = Pick<
+  Parameters<WorkflowAIApi['tasks']['schemas']['runs']['import']>[0],
+  'id' | 'group' | 'start_time' | 'end_time' | 'labels'
+>
 
 export class WorkflowAI {
   protected api: WorkflowAIApi
@@ -38,8 +45,31 @@ export class WorkflowAI {
     }
   }
 
+  protected async upsertTask<IS extends InputSchema, OS extends OutputSchema>(
+    taskDef: TaskDefinition<IS, OS, true>,
+  ): Promise<TaskDefinition<IS, OS, false>> {
+    const { data } = await this.api.tasks.upsert({
+      task_id: taskDef.taskId,
+      name: taskDef.taskName || taskDef.taskId,
+      // @ts-expect-error The generated API types are messed up
+      input_schema: await inputZodToSchema(taskDef.schema.input),
+      // @ts-expect-error The generated API types are messed up
+      output_schema: await outputZodToSchema(taskDef.schema.output),
+    })
+
+    return {
+      ...taskDef,
+      taskId: data.task_id!,
+      taskName: data.name,
+      schema: {
+        ...taskDef.schema,
+        id: data.task_schema_id!,
+      },
+    }
+  }
+
   protected async runTask<IS extends InputSchema, OS extends OutputSchema>(
-    taskDef: TaskDefinition<IS, OS>,
+    taskDef: TaskDefinition<IS, OS, false>,
     input: IS,
     options: RunTaskOptions,
   ): Promise<TaskOutput<OS>> {
@@ -53,13 +83,49 @@ export class WorkflowAI {
     return taskDef.schema.output.parseAsync(data.task_output)
   }
 
+  protected async importTaskRun<
+    IS extends InputSchema,
+    OS extends OutputSchema,
+  >(
+    taskDef: TaskDefinition<IS, OS, false>,
+    input: IS,
+    output: OS,
+    options: ImportTaskRunOptions,
+  ) {
+    const [task_input, task_output] = await Promise.all([
+      taskDef.schema.input.parseAsync(input),
+      taskDef.schema.output.parseAsync(output),
+    ])
+
+    const { data } = await this.api.tasks.schemas.runs.import({
+      ...options,
+      task_id: taskDef.taskId.toLowerCase(),
+      task_schema_id: taskDef.schema.id,
+      task_input,
+      task_output,
+    })
+
+    return data
+  }
+
   public async compileTask<IS extends InputSchema, OS extends OutputSchema>(
-    taskDef: TaskDefinition<IS, OS>,
+    _taskDef: TaskDefinition<IS, OS, true>,
     defaultOptions?: Partial<RunTaskOptions>,
   ): Promise<ExecutableTask<IS, OS>> {
-    // TODO: register task
+    let taskDef: TaskDefinition<IS, OS>
 
-    return (input, overrideOptions) => {
+    // Make sure we have a schema ID, either passed or by upserting the task
+    if (hasSchemaId(_taskDef)) {
+      taskDef = _taskDef
+    } else if (_taskDef.taskName) {
+      taskDef = await this.upsertTask(_taskDef)
+    } else {
+      throw new Error(
+        'Invalid task definition to compile: missing task schema id or task name',
+      )
+    }
+
+    const runTask: ExecutableTask<IS, OS> = (input, overrideOptions) => {
       const options = {
         ...defaultOptions,
         ...overrideOptions,
@@ -67,5 +133,22 @@ export class WorkflowAI {
 
       return this.runTask<IS, OS>(taskDef, input, options)
     }
+
+    runTask.importRun = async (input, output, overrideOptions) => {
+      return this.importTaskRun(taskDef, input, output, {
+        ...defaultOptions,
+        ...overrideOptions,
+        group: {
+          ...defaultOptions?.group,
+          ...overrideOptions?.group,
+          properties: {
+            ...defaultOptions?.group?.properties,
+            ...overrideOptions?.group?.properties,
+          },
+        },
+      })
+    }
+
+    return runTask
   }
 }
