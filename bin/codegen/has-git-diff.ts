0124400b8c60bc6fc69ed9331e676c36c0db2b5e
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-28 18:05:59 -0400

    chore: log error for action script

--- bin/codegen/has-git-diff.ts
+++ bin/codegen/has-git-diff.ts
@@ -2,19 +2,25 @@ import * as core from '@actions/core'
 import assert from 'node:assert'
 import { spawnSync } from 'node:child_process'
 
-// Check if there are any git diff pending
-const gitDiffCmd = spawnSync('git', ['diff'])
-assert(!gitDiffCmd.error, gitDiffCmd.error)
-assert(!gitDiffCmd.stderr.toString(), `Failed to run git diff: ${gitDiffCmd.stderr.toString()}`)
+try {
+  // Check if there are any git diff pending
+  const gitDiffCmd = spawnSync('git', ['diff'])
+  assert(!gitDiffCmd.error, gitDiffCmd.error)
+  assert(!gitDiffCmd.stderr.toString(), `Failed to run git diff: ${gitDiffCmd.stderr.toString()}`)
 
-const diffResult = gitDiffCmd.stdout.toString()
-const hasDiff: boolean = diffResult.length > 0
+  const diffResult = gitDiffCmd.stdout.toString()
+  const hasDiff: boolean = diffResult.length > 0
 
-if (hasDiff) {
-  console.log('Git changes are pending')
+  if (hasDiff) {
+    console.log('Git changes are pending')
+  }
+  else {
+    console.log('No git changes')
+  }
+
+  core.setFailed('No git changes')
 }
-else {
-  console.log('No git changes')
+catch (error) {
+  console.error(error)
+  throw error
 }
-
-core.setFailed('No git changes')
