d49f75d5441c118241841590ba70e8cd732c245e
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-30 07:04:51 -0400

    chore: Move helper file

--- packages/schema/src/schemaToZod.ts
+++ packages/schema/src/schemaToZod.ts
@@ -5,7 +5,7 @@ import {
 } from 'json-schema-to-zod'
 
 import { Definition, definitions } from './definitions'
-import { hydrateRefs } from './json-schema-refs'
+import { hydrateRefs } from './json-schema/json-schema-refs'
 
 /**
  * Creates a parser override function for a specific type of Zod schema.
