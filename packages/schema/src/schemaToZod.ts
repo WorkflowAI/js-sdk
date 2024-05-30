3e234889a4155aeb66806cf949d79f557e984837
Pierre-Elie Fauché <github@pierre-elie.me>
2024-05-30 07:05:45 -0400

    feat: sanitize json schema by adding missing types

--- packages/schema/src/schemaToZod.ts
+++ packages/schema/src/schemaToZod.ts
@@ -6,6 +6,7 @@ import {
 
 import { Definition, definitions } from './definitions'
 import { hydrateRefs } from './json-schema/json-schema-refs'
+import { sanitize } from './json-schema/sanitize-json-schema'
 
 /**
  * Creates a parser override function for a specific type of Zod schema.
@@ -41,7 +42,9 @@ const schemaToZod = async (
   jsonSchema: JsonSchemaObject,
   parserOverride: ParserOverride,
 ): Promise<string> => {
-  const resolvedJsonSchema = hydrateRefs(jsonSchema, { keepRefs: true })
+  const resolvedJsonSchema = hydrateRefs(sanitize(jsonSchema), {
+    keepRefs: true,
+  })
   return jsonSchemaToZod(resolvedJsonSchema, { parserOverride })
 }
 
