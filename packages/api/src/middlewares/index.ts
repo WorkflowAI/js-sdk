64966abaca5d18d3d4835463901f15403951d3bf
Spatika Ganesh <spatika@workflowai.com>
2024-09-19 10:07:44 -0700

    Updates to feedback

--- packages/api/src/middlewares/index.ts
+++ packages/api/src/middlewares/index.ts
@@ -3,12 +3,22 @@ import { Middleware } from 'openapi-fetch'
 
 export * from './throwError.js'
 export type { Middleware } from 'openapi-fetch'
+
+function getPackageVersion() {
+  try {
+    const packageJson = JSON.parse(readFileSync('package.json', 'utf8'))
+    return packageJson?.version;
+  } catch (error) {
+    console.error('Error reading package.json:', error)
+    return undefined
+  }
+}
+
 const customHeaders: Middleware = {
   onRequest: (req) => {
     req.headers.set('x-workflowai-source', 'sdk')
     req.headers.set('x-workflowai-language', 'typescript')
-    const packageJson = JSON.parse(readFileSync('package.json', 'utf8'))
-    req.headers.set('x-workflowai-version', packageJson.version)
+    req.headers.set('x-workflowai-version', getPackageVersion())
     return req
   },
 }
