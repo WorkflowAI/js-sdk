321d95a0311b38a3e2d86b1466f2ac60e0c0086b
Guillaume Aquilina <guillaume@workflowai.com>
2024-10-10 16:26:09 -0400

    fix: typo

--- packages/schema/src/atoms/file.test.ts
+++ packages/schema/src/atoms/file.test.ts
@@ -4,7 +4,7 @@ describe('FILE', () => {
   it('should validate a valid FILE input object', () => {
     const validInput = {
       name: 'FILE.png',
-      content_type: 'FILE/png',
+      content_type: 'image/png',
       data: 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==', // a 1x1 red dot
     };
 
@@ -21,7 +21,7 @@ describe('FILE', () => {
 
   it('should validate an FILE input object with optional name', () => {
     const validInput = {
-      content_type: 'FILE/jpeg',
+      content_type: 'image/jpeg',
       data: 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==', // a 1x1 red dot
     };
 
@@ -40,7 +40,7 @@ describe('FILE', () => {
   it('should fail validation for invalid content_type', () => {
     const invalidInput = {
       name: 'FILE.gif',
-      content_type: 'FILE/svg',
+      content_type: 'image/svg',
       data: 'base64encodeddata',
     };
 
@@ -50,7 +50,7 @@ describe('FILE', () => {
   it('should fail validation for missing data', () => {
     const invalidInput = {
       name: 'FILE.jpg',
-      content_type: 'FILE/jpeg',
+      content_type: 'image/jpeg',
     };
 
     expect(FILE.safeParse(invalidInput).success).toBe(false);
@@ -59,7 +59,7 @@ describe('FILE', () => {
   it('should fail validation for invalid data', () => {
     const invalidInput = {
       name: 'FILE.png',
-      content_type: 'FILE/png',
+      content_type: 'image/png',
       data: 12345,
     };
 
@@ -69,7 +69,7 @@ describe('FILE', () => {
   it('allows any string as content_type', () => {
     const validInput = {
       name: 'FILE.png',
-      content_type: 'FILE/png',
+      content_type: 'image/png',
       data: 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==', // a 1x1 red dot
     };
 
