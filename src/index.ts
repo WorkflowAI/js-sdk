90784feda561555c2e5dadbf495a7b9099ef99b5
Guillaume Aquilina <guillaume@workflowai.com>
2025-02-14 14:27:32 -0500

    fix: export agent types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export type * from './types.js';
 export type { TaskInput, TaskOutput } from './task.js';
 export { WorkflowAIError } from './api/error.js';
 export * as z from './schema/zod/zod.js';
+export type * from './agent.js';
