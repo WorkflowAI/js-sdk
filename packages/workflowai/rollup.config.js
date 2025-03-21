cc37af815c97e1b1df018983b7863e79e49d99ea
Guillaume Aquilina <guillaume@workflowai.com>
2025-03-21 15:37:07 -0400

    feat: remove unused files

---
+++ packages/workflowai/rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts', // entry point of your library
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(), // automatically externalize peerDependencies in package.json
+    resolve(), // resolves node_modules packages
+    commonjs(), // converts CommonJS modules to ES6
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
